36191f3c2e4a6c6e415143adeedadb7acb58cf1b
Sandeep Thilakan <sandeepthilakan@Sandeeps-MacBook-Pro.local>
2018-02-25 14:12:02 +0530

    first commit

---
+++ src/main/java/blog/model/Post.java
@@ -0,0 +1,53 @@
+package blog.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Post implements Serializable {
+
+    private Long id;
+    private String title;
+    private String body;
+    private Date date = new Date();
+
+    public Post() {
+    }
+
+    public Post(Long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
---
+++ src/main/java/blog/model/User.java
@@ -0,0 +1,38 @@
+package blog.model;
+
+public class User {
+
+    private String userName;
+    private String passwordHash;
+    private String fullName;
+
+    public User(String userName, String fullName) {
+        this.userName = userName;
+        this.fullName = fullName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
